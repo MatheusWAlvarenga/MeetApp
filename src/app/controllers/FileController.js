cf02362f1a7f3d945c21920f8db224fd9becb7a6
MatheusWilliam <matheuswalvarenga@gmail.com>
2019-11-22 01:22:31 -0300

    MeetApp

---
+++ src/app/controllers/FileController.js
@@ -0,0 +1,18 @@
+import File from '../models/File'
+
+class FileController {
+  async store(req, res) {
+   const { originalname: name, filename: path } = req.file
+   const { id: avatar_id } = req.file
+
+   const file = await File.create({
+     name,
+     path,
+    avatar_id,
+   })
+
+   return res.json(file)
+  }
+}
+
+export default new FileController()
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    secure: false,
+    auth: {
+      user: "95c945c0402352",
+      pass: "e451d509865021",
+    },
+  default:{
+    from: 'Equipe MeetApp <noreply@meetapp.com>'
+  }
+}
